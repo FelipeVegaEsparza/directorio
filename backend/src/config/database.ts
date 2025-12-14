510a660e3a7194b85912383f20724deceb012986
Felipe <felipevegaesparza@gmail.com>
2025-12-14 20:24:39 -0300

    Fix: Prioritize production env vars over dotenv

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -1,7 +1,9 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config();
+}
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
